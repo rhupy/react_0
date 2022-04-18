ed06a3c9d642dda90e093ef8dd2a6a024fea35ec
rhupy1 <fmsj@naver.com>
2022-04-18 17:48:04 +0900

    인프런 04-18

---
+++ infrun-react/hello-world/jsx.js
@@ -0,0 +1,7 @@
+<div className="box">
+    <Title text="hello world" width={200}/>
+    <button onClick= {()=>{}} >좋아요</button>
+    <a href="/home" style={ { marginTop:'10px', color:'red'} }>
+        홈으로 이동
+    </a>
+</div>
---
+++ infrun-react/hello-world/simple1_jsx.js
@@ -0,0 +1,47 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? "좋아요 취소" : "좋아요";
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text); // return React.createElement(
+  //     'button',
+  //     { onClick: () => setLiked(!liked) },
+  //     text
+  // );
+}
+
+function Container() {
+  const [count, setCount] = React.useState(2); // return (
+  //     <div>
+  //         <LikeButton />
+  //         <div>
+  //             <span>현재 카운트: </span>
+  //             <span style={{marginRight: 10 }}>{count}</span>
+  //             <button onClick={() => setCount(count + 1)}>증가</button>
+  //             <button onClick={() => setCount(count - 1)}>감소</button>
+  //         </div>
+  //     </div>
+  // );
+
+  return React.createElement('div', null, React.createElement(LikeButton), React.createElement('div', {
+    style: {
+      marginTop: 20,
+      color: 'red'
+    }
+  }, React.createElement('span', null, '현재 카운트: '), React.createElement('span', {
+    style: {
+      marginRight: 10
+    }
+  }, count), React.createElement('button', {
+    onClick: () => setCount(count + 1)
+  }, '증가'), React.createElement('button', {
+    onClick: () => setCount(count - 1)
+  }, '감소')));
+}
+
+;
+const domContainer1 = document.getElementById('root1');
+ReactDOM.render(React.createElement('div', null, //React.createElement(LikeButton),
+//React.createElement(LikeButton),
+//React.createElement(LikeButton),
+React.createElement(Container)), domContainer1);
