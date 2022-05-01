560c17500b5ac66622b1690d7b2cda425be04151
rhupy1 <fmsj@naver.com>
2022-05-01 18:05:33 +0900

    commit before yarn eject

--- hello-react/9장 컴포넌트 스타일링/styling-react/src/SassComponent.js
+++ hello-react/9장 컴포넌트 스타일링/styling-react/src/SassComponent.js
@@ -6,6 +6,7 @@ const SassComponent = () => {
             <div className="box red" />
             <div className="box orange" />
             <div className="box yellow" />
+
             <div className="box green" />
             <div className="box blue" />
             <div className="box indigo" />
